3074eed02a87da3b03f0ff2804198fe18536d19b
Honghe <leopardsaga@gmail.com>
2018-05-14 17:18:34 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// file my_extension/main.js
+define([
+    'base/js/namespace',
+    'nbextensions/mermaid_extension/mermaid'
+], function(
+    Jupyter,
+    mermaid
+) {
+    function loadScript() {
+        mermaid.initialize({startOnLoad:true});   
+        console.log("hello mermaid")
+    }
+    
+    function load_ipython_extension() {
+        console.log(
+            'This is the current notebook application instance:',
+            Jupyter.notebook
+        );
+        loadScript();
+    }
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
